85453094f654869092e65871b0869793a4d50048
Alan Justino <alan.justino@yahoo.com.br>
2017-01-12 19:32:04 -0200

    Most simple types are implemented now

--- src/pyrqlite/extensions.py
+++ src/pyrqlite/extensions.py
@@ -19,21 +19,25 @@ def _decoder(conv_func):
     return lambda s: conv_func(s.decode('utf-8'))
 
 
+adapters = {
+    float: lambda x: x,
+    int: lambda x: x,
+    bool: lambda x: int(x),
+}
 converters = {
     'TEXT': str,
     'VARCHAR': lambda x: x.encode('utf-8'),
     'INTEGER': int,
+    'BOOL': bool,
+    'FLOAT': float,
     'REAL': float,
     'NULL': lambda x: None,
-    'BLOB': lambda x: x.decode('base64'),
+    'BLOB': lambda x: x,
     '': lambda x: x.encode('utf-8'),
 }
 
-
-adapters = {
-    bool: int,
-    #str: lambda x: x.decode('utf-8')
-}
+# Non-native converters will be decoded from base64 before fed into converter
+_native_converters = ('BOOL', 'FLOAT', 'INTEGER', 'REAL', 'NUMBER', 'TEXT', 'VARCHAR', 'NULL', '')
 
 
 def register_converter(type_string, value_string):
@@ -45,22 +49,29 @@ def register_adapter(type_, function):
 
 
 def _convert_to_python(type_, value):
-    if type_.upper() in converters:
-        return converters[type_.upper()](value)
+    type_upper = type_.upper()
+    if type_upper in converters:
+        if type_upper not in _native_converters:
+            value = value.decode('base64')
+        return converters[type_upper](value)
     return value
 
 
 def _adapt_from_python(value):
-    if isinstance(value, basestring):
-        escaped = value.replace(b"'", b"''")
-        adapted = (type(value)(b"'%s'")) % escaped
-    else:
-        if isinstance(value, numbers.Number):
-            adapted = value
-        else:
+    if not isinstance(value, basestring):
+        try:
             adapted = adapters[type(value)](value)
+    else:
+        adapted = value
 
-    if not isinstance(adapted, basestring):
+    # The adapter could had returned a string
+    if isinstance(adapted, basestring):
+        adapted = _escape_string(adapted)
+    else:
         adapted = str(adapted)
 
     return adapted
+
+
+def _escape_string(value):
+    return (type(value)(b"'%s'")) % (value.replace(b"'", b"''"))
