rqlite/pyrqlite

commit 0256d5fde135724bcb0a2c91d12bb2ca2d65fa45

Implicit cursor on Connection.execute

src/pyrqlite/connections.py
@@ -94,3 +94,8 @@ class Connection(object):
     def cursor(self):
         """Return a new Cursor Object using the connection."""
         return Cursor(self)
+
+    def execute(self, *args, **kwargs):
+        with self.cursor() as cursor:
+            return cursor.execute(*args, **kwargs)
+

commit ded8417a7ae68510feb7a1eee38e0c41f685412b

Connection.execute: don't call Cursor.__exit__ before return
This fixes the following ExtensionTests.test_CheckConnectionExecute
failure:

======================================================================
ERROR: test_CheckConnectionExecute (test_dbapi.ExtensionTests)
----------------------------------------------------------------------
Traceback (most recent call last):
  File "test/test_dbapi.py", line 772, in test_CheckConnectionExecute
    result = self.con.execute("select 5").fetchone()[0]
TypeError: 'NoneType' object is not subscriptable

Fixes: 0256d5fde135 ("Implicit cursor on Connection.execute")

src/pyrqlite/connections.py
@@ -109,6 +109,5 @@ class Connection(object):
         return Cursor(self)
 
     def execute(self, *args, **kwargs):
-        with self.cursor() as cursor:
-            return cursor.execute(*args, **kwargs)
+        return self.cursor().execute(*args, **kwargs)
 