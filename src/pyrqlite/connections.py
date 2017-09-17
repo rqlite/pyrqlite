rqlite/pyrqlite

commit ad4120543f907ee1dd382f7979a1eb3ebc4a8ea1

Connection: add :memory: support

src/pyrqlite/connections.py
@@ -20,6 +20,7 @@ from .constants import (
 )
 
 from .cursors import Cursor
+from ._ephemeral import EphemeralRqlited as _EphemeralRqlited
 from .extensions import PARSE_DECLTYPES, PARSE_COLNAMES
 
 
@@ -38,7 +39,7 @@ class Connection(object):
         NotSupportedError,
     )
 
-    def __init__(self, host='localhost', port=4001, connect_timeout=None,
+    def __init__(self, host=':memory:', port=4001, connect_timeout=None,
                  detect_types=0, max_redirects=UNLIMITED_REDIRECTS):
 
         self.messages = []
@@ -49,6 +50,10 @@ class Connection(object):
         self.detect_types = detect_types
         self.parse_decltypes = detect_types & PARSE_DECLTYPES
         self.parse_colnames = detect_types & PARSE_COLNAMES
+        self._ephemeral = None
+        if host == ':memory:':
+            self._ephemeral = _EphemeralRqlited().__enter__()
+            self.host, self.port = self._ephemeral.http
         self._connection = self._init_connection()
 
     def _init_connection(self):
@@ -95,6 +100,12 @@ class Connection(object):
         a connection without committing the changes first will cause an
         implicit rollback to be performed."""
         self._connection.close()
+        if self._ephemeral is not None:
+            self._ephemeral.__exit__(None, None, None)
+            self._ephemeral = None
+
+    def __del__(self):
+        self.close()
 
     def commit(self):
         """Database modules that do not support transactions should

src/pyrqlite/_ephemeral.py
@@ -1,0 +1,79 @@
+
+import contextlib
+import os
+import shutil
+import socket
+import subprocess
+import tempfile
+import time
+
+
+class EphemeralRqlited(object):
+    def __init__(self):
+        self.host = None
+        self.http = None
+        self.raft = None
+        self._tempdir = None
+        self._proc = None
+
+    @staticmethod
+    def _unused_ports(host, count):
+        sockets = []
+        ports = []
+        try:
+            sockets.extend(
+                socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                for i in range(count))
+            for s in sockets:
+                s.bind((host, 0))
+                ports.append(s.getsockname()[-1])
+        finally:
+            while sockets:
+                sockets.pop().close()
+
+        return ports
+
+    @staticmethod
+    def _test_port(host, port, timeout=None):
+        try:
+            with contextlib.closing(
+                socket.create_connection((host, port), timeout=timeout)):
+                return True
+        except socket.error:
+            return False
+
+    def _start(self):
+        self._tempdir = tempfile.mkdtemp()
+        self.host = 'localhost'
+
+        # Allocation of unused ports is racy, so retry
+        # until ports have been successfully acquired.
+        while self._proc is None:
+            http_port, raft_port = self._unused_ports(self.host, 2)
+            self.http = (self.host, http_port)
+            self.raft = (self.host, raft_port)
+            with open(os.devnull, mode='wb', buffering=0) as devnull:
+                self._proc = subprocess.Popen(['rqlited',
+                    '-http-addr', '{}:{}'.format(*self.http),
+                    '-raft-addr', '{}:{}'.format(*self.raft), self._tempdir],
+                    stdout=devnull, stderr=devnull)
+
+            while not self._test_port(*self.http) and self._proc.poll() is None:
+                time.sleep(0.5)
+
+            if self._proc.poll() is not None:
+                self._proc = None
+
+    def __enter__(self):
+        self._start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if self._tempdir is not None:
+            shutil.rmtree(self._tempdir)
+            self._tempdir = None
+        if self._proc is not None:
+            self._proc.terminate()
+            self._proc.wait()
+            self._proc = None
+        return False

commit 86906b47c20e5aca21f07876bc253ee1449a0139

Connection: revert default host to localhost
Fixes: ad4120543f90 ("Connection: add :memory: support")

src/pyrqlite/connections.py
@@ -39,7 +39,7 @@ class Connection(object):
         NotSupportedError,
     )
 
-    def __init__(self, host=':memory:', port=4001, connect_timeout=None,
+    def __init__(self, host='localhost', port=4001, connect_timeout=None,
                  detect_types=0, max_redirects=UNLIMITED_REDIRECTS):
 
         self.messages = []