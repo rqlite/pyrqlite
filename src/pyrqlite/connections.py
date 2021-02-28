rqlite/pyrqlite

commit c47b01b9bf961c23a8f8e006da6ea85e87cba947

Add support for HTTPS connection (fixes #29)

src/pyrqlite/connections.py
@@ -5,10 +5,10 @@ import codecs
 import logging
 
 try:
-    from http.client import HTTPConnection
+    from http.client import HTTPConnection, HTTPSConnection
 except ImportError:
     # pylint: disable=import-error
-    from httplib import HTTPConnection
+    from httplib import HTTPConnection, HTTPSConnection
 
 try:
     from urllib.parse import urlparse
@@ -40,11 +40,12 @@ class Connection(object):
         NotSupportedError,
     )
 
-    def __init__(self, host='localhost', port=4001,
+    def __init__(self, scheme='http', host='localhost', port=4001,
                  user=None, password=None, connect_timeout=None,
                  detect_types=0, max_redirects=UNLIMITED_REDIRECTS):
 
         self.messages = []
+        self.scheme = scheme
         self.host = host
         self.port = port
         self._headers = {}
@@ -65,8 +66,14 @@ class Connection(object):
         self._connection = self._init_connection()
 
     def _init_connection(self):
-        return HTTPConnection(self.host, port=self.port,
-                              timeout=None if self.connect_timeout is None else float(self.connect_timeout))
+        if self.scheme == 'http':
+            cls = HTTPConnection
+        elif self.scheme == 'https':
+            cls = HTTPSConnection
+        else:
+            raise Connection.ProgrammingError('Unsupported scheme %r' % self.scheme)
+        return cls(self.host, port=self.port,
+                   timeout=None if self.connect_timeout is None else float(self.connect_timeout))
 
     def _retry_request(self, method, uri, body=None, headers={}):
         tries = 10

commit 50a5201ce3a229499a413b50cc16f51a9c16255d

connection: :memory: argument for unit tests
Fixes: c47b01b9bf96 ("Add support for HTTPS connection (fixes #29)")

src/pyrqlite/connections.py
@@ -60,13 +60,13 @@ class Connection(object):
         self.parse_decltypes = detect_types & PARSE_DECLTYPES
         self.parse_colnames = detect_types & PARSE_COLNAMES
         self._ephemeral = None
-        if host == ':memory:':
+        if scheme == ':memory:':
             self._ephemeral = _EphemeralRqlited().__enter__()
             self.host, self.port = self._ephemeral.http
         self._connection = self._init_connection()
 
     def _init_connection(self):
-        if self.scheme == 'http':
+        if self.scheme in ('http', ':memory:'):
             cls = HTTPConnection
         elif self.scheme == 'https':
             cls = HTTPSConnection