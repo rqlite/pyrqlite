rqlite/pyrqlite

commit 5b5fb66f12127dd5d7042c4afb5f4da6c99633e7

Adding support for named parameters

src/pyrqlite/cursors.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 import json
 import logging
 import sys
+import re
 
 try:
     # pylint: disable=no-name-in-module
@@ -90,16 +91,53 @@ class Cursor(object):
         '''
         SQLite natively supports only the types TEXT, INTEGER, REAL, BLOB and NULL
         '''
+
+        param_matches = 0
         subst = []
-        parts = operation.split('?')
-        if len(parts) - 1 != len(parameters):
-            raise ProgrammingError('incorrect number of parameters (%s != %s): %s %s' %
-                                   (len(parts) - 1, len(parameters), operation, parameters))
-        for i, part in enumerate(parts):
-            subst.append(part)
-            if i < len(parameters):
-                subst.append(_adapt_from_python(parameters[i]))
-        return ''.join(subst)
+
+        qmark_re = re.compile(r"(\?)")
+        named_re = re.compile(r"(:{1}[a-zA-Z]+?\b)")
+
+        qmark_matches = qmark_re.findall(operation)
+        named_matches = named_re.findall(operation)
+
+        if len(qmark_matches) > 0 and len(named_matches) > 0:
+            raise ProgrammingError('different paramater types in operation not permitted: %s %s' %
+                                   (operation, parameters))
+
+        param_matches = len(qmark_matches) + len(named_matches)
+
+        # Matches but no parameters
+        if param_matches > 0 and parameters == None:
+            raise ProgrammingError('paramater required but not given: %s' % operation)
+
+        # No regex matches and no parameters.
+        if parameters == None:
+            return operation
+
+        if isinstance(parameters,dict):
+            # parameters is a dict or a dict subclass
+            if len(qmark_matches) > 0:
+                raise ProgrammingError('Unamed binding used, but you supplied a dictionary (which has only names): %s %s' %
+                                       (operation, parameters))
+            for op_key in named_matches:
+                try:
+                    operation = operation.replace(op_key, _adapt_from_python(parameters[op_key[1:]]))
+                except KeyError:
+                    raise ProgrammingError('the named parameters given do not match operation: %s %s' %
+                                           (operation, parameters)) 
+        else:
+            # parameters is a sequence
+            if param_matches != len(parameters):
+                raise ProgrammingError('incorrect number of parameters (%s != %s): %s %s' %
+                                   (param_matches, len(parameters), operation, parameters))
+            if len(named_matches) > 0:
+                raise ProgrammingError('Named binding used, but you supplied a sequence (which has no names): %s %s' %
+                                       (operation, parameters))
+            for i in range(len(parameters)):
+                operation = operation.replace('?', _adapt_from_python(parameters[i]),1)
+ 
+        return operation
 
     def _get_sql_command(self, sql_str):
         return sql_str.split(None, 1)[0].upper()
@@ -110,8 +148,7 @@ class Cursor(object):
                 "argument must be a string, not '{}'".format(
                 type(operation).__name__))
 
-        if parameters:
-            operation = self._substitute_params(operation, parameters)
+        operation = self._substitute_params(operation, parameters)
 
         command = self._get_sql_command(operation)
         if command in ('SELECT', 'PRAGMA'):

commit b0bc6ab7e0eee07b1dfa5bc511046adefe07ead6

_substitute_params: restore parameter sequence code from prior to 5b5fb66f1212
Restore parameter sequence code from prior to commit
5b5fb66f1212. This unit test case demonstrates the problem
reported in https://github.com/rqlite/pyrqlite/issues/30:

    def test_CheckRowcountExecute(self):
        self.cu.execute("delete from test")
        self.cu.execute("insert into test(name, income) values (?, ?)", ("?", "1"))
        self.cu.execute("select name from test where name=?", ("?",))
        self.assertEqual(self.cu.rowcount, 1,
            msg="test failed for https://github.com/rqlite/pyrqlite/issues/30")

src/test/test_dbapi.py:346:
_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _

self = <pyrqlite.cursors.Cursor object at 0x7f16e1abc400>, operation = "insert into test(name, income) values (''1'', ?)", parameters = ('?', '1')

    def execute(self, operation, parameters=None):
        if not isinstance(operation, basestring):
            raise ValueError(
                             "argument must be a string, not '{}'".format(type(operation).__name__))

        operation = self._substitute_params(operation, parameters)

        command = self._get_sql_command(operation)
        if command in ('SELECT', 'PRAGMA'):
            payload = self._request("GET",
                                    "/db/query?" + _urlencode({'q': operation}))
        else:
            payload = self._request("POST", "/db/execute?transaction",
                                    headers={'Content-Type': 'application/json'}, body=json.dumps([operation]))

        last_insert_id = None
        rows_affected = -1
        payload_rows = {}
        try:
            results = payload["results"]
        except KeyError:
            pass
        else:
            rows_affected = 0
            for item in results:
                if 'error' in item:
                    logging.getLogger(__name__).error(json.dumps(item))
>                   raise Error(json.dumps(item))
E                   sqlite3.Error: {"error": "near \"1\": syntax error"}

pyrqlite/cursors.py:178: Error

Note that a similar problem still exists for named
parameters, so https://github.com/rqlite/pyrqlite/issues/30
is not entirely fixed.

Reported-by: @jaysonlarose
See: https://github.com/rqlite/pyrqlite/issues/30
Fixes: 5b5fb66f1212

src/pyrqlite/cursors.py
@@ -139,9 +139,13 @@ class Cursor(object):
                 raise ProgrammingError('Named binding used, but you supplied a'
                                        ' sequence (which has no names): %s %s' %
                                        (operation, parameters))
-            for i in range(len(parameters)):
-                operation = operation.replace('?', 
-                                              _adapt_from_python(parameters[i]), 1)
+            parts = operation.split('?')
+            subst = []
+            for i, part in enumerate(parts):
+                subst.append(part)
+                if i < len(parameters):
+                    subst.append(_adapt_from_python(parameters[i]))
+            operation = ''.join(subst)
 
         return operation
 

src/test/test_dbapi.py
@@ -343,7 +343,10 @@ class CursorTests(unittest.TestCase):
 
     def test_CheckRowcountExecute(self):
         self.cu.execute("delete from test")
-        self.cu.execute("insert into test(name) values ('foo')")
+        self.cu.execute("insert into test(name, income) values (?, ?)", ("?", "1"))
+        self.cu.execute("select name from test where name=?", ("?",))
+        self.assertEqual(self.cu.rowcount, 1,
+            msg="test failed for https://github.com/rqlite/pyrqlite/issues/30")
         self.cu.execute("insert into test(name) values ('foo')")
         self.cu.execute("update test set name='bar'")
         self.assertEqual(self.cu.rowcount, 2)